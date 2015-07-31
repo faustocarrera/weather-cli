cbbda0b21567701ed5991136dd19eb8c6fe22613
Fausto Carrera <fausto.carrera.f@gmail.com>
2015-07-30 22:26:47 -0300

    display current weather and 8 days forecast

--- README.md
+++ README.md
@@ -4,16 +4,44 @@ Weather from the command line
 ## How this works?
 Basically the script checks your ip, geolocate the ip and checks the weather based on that information, so cool!
 
-## Webservices
-**Forecast.io**  
-[Developers](https://developer.forecast.io/)  
-[Docs](https://developer.forecast.io/docs/v2)
+## Data sources
+
+[Forecast.io](https://developer.forecast.io/)   
+[GeoLite2 Free download](https://dev.maxmind.com/geoip/geoip2/geolite2/)  
+[GeoLite2 City MaxMin DB](http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz)
+
 
 ## Requirements
 
-Installrequirements  
+* [requests](http://docs.python-requests.org/en/latest/) >= 2.4
+* [maxminddb](https://github.com/maxmind/libmaxminddb) >= 1.1
+* [geoip2](https://pypi.python.org/pypi/geoip2) >= 2.2
+* [tabletext](https://github.com/Thibauth/tabletext) >= 0.1
 
+Install requirements  
 
 ```python
 pip install -r requirements.txt
 ```
+
+You gonna need a Forecast.io API key, register [here](https://developer.forecast.io/) to obtain it
+
+## Configuration
+
+Just update the config/wheather.conf file with your Forecast.io API key and you are ready.
+
+## Try it
+
+* git clone git@github.com:faustocarrera/weather-cli.git
+* update the config/wheather.conf file with your Forecast.io API key
+* virtualenv env
+* source env/bin/activate
+* pip install -r requirements.txt
+* python weather.py
+* profit
+
+## Todo
+
+* add option for just current weather
+* add option for next 12 hours forecast
+* add option for next days forecast
--- weather.py
+++ weather.py
@@ -1,2 +1,124 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import os
+import sys
+import ConfigParser
+import requests
+import geoip2.database
+import json
+import datetime
+from tabletext import to_text
+
+
+def get_ip(ip_url):
+    "check the external ip"
+    result = {'status': 'ok', 'ip': 0}
+    try:
+        headers = {'Content-type': 'application/json'}
+        req = requests.get(
+            ip_url,
+            headers=headers,
+        )
+        result['ip'] = req.text
+    except Exception, error:
+        sys.exit(error)
+    return result
+
+
+def get_geolocation(ipaddress):
+    "check latitude and longitude of the ip"
+    result = {'status': 'ok', 'lat': 0, 'lon': 0}
+    reader = geoip2.database.Reader('./database/GeoLite2-City.mmdb')
+    response = reader.city(ipaddress)
+    try:
+        result['city'] = str(response.city.name)
+        result['lat'] = float(response.location.latitude)
+        result['lon'] = float(response.location.longitude)
+    except ValueError as error:
+        sys.exit(error[0])
+    except geoip2.errors.AddressNotFoundError as error:
+        sys.exit(error[0])
+    reader.close()
+    return result
+
+
+def get_weather(config, geolocation):
+    "request weather based on the location"
+    forecast_url = '%s/%s/%s,%s/%s' % (
+        config['url'],
+        config['key'],
+        geolocation['lat'],
+        geolocation['lon'],
+        '?units=si'
+    )
+    headers = {'Content-type': 'application/json'}
+    # force disable insecure request warning
+    requests.packages.urllib3.disable_warnings()
+    req = requests.get(
+        forecast_url,
+        headers=headers,
+    )
+    try:
+        return json.loads(req.text)
+    except (ValueError, KeyError, TypeError):
+        sys.exit(req.json())
+
+
+def output(weather, city):
+    table = []
+    table.append(['Weather for %s' % city])
+    # header
+    table.append(['date/time', 'summary', 'temp', 'term', 'humidity'])
+    # current weather
+    table.append([format_timestamp(weather['currently']['time']),
+                  weather['currently']['summary'],
+                  weather['currently']['temperature'],
+                  weather['currently']['apparentTemperature'],
+                  float(weather['currently']['humidity'])])
+    # daily
+    table.append(['Forecast next %s days' % len(weather['daily']['data'])])
+    table.append(['date/time', 'summary', 'min', 'max', 'humidity'])
+    for data in weather['daily']['data']:
+        table.append([format_timestamp(data['time']),
+                      data['summary'],
+                      data['temperatureMin'],
+                      data['temperatureMax'],
+                      data['humidity']])
+    print to_text(table, header=False, corners='+', hor='-', ver='|', formats=['', '', '>', '>', '>', '>'])
+
+
+def format_timestamp(ts):
+    "transform timestamp to datetime"
+    return datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M')
+
+
+def load_config():
+    script_path = os.path.abspath(os.path.dirname(sys.argv[0]))
+    if not script_path:
+        script_path = os.path.abspath('.')
+    filename = r'%s/config/weather.conf' % script_path
+    config_parser = ConfigParser.RawConfigParser()
+    config_parser.read(filename)
+    config = {
+        'forecast': {
+            'url': config_parser.get('forecast', 'url'),
+            'key': config_parser.get('forecast', 'key')
+        },
+        'ip': {
+            'url': config_parser.get('ip', 'url')
+        }
+    }
+    return config
+
+
+def main():
+    config = load_config()
+    ip = get_ip(config['ip']['url'])
+    geo = get_geolocation(ip['ip'])
+    weather = get_weather(config['forecast'], geo)
+    output(weather, geo['city'])
+
+
+if __name__ == '__main__':
+    main()
